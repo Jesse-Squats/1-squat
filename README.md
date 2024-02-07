949c02018c37c14776016f2c3e238393f8c2e77f
ivcained <86070833+ivcained@users.noreply.github.com>
2024-02-07 13:11:39 +0530

    chore: basic lineups and base onchain kit integration

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Farcaster Frames Poll app
+# 1 SQUAT
 
 A example Poll app using [Farcaster Frames](https://warpcast.notion.site/Farcaster-Frames-4bd47fe97dc74a42a48d3a234636d8c5). 
 
@@ -8,13 +8,3 @@ so the votes cannot be spoofed (if `HUB_URL` is provided), and the results are s
 
 ## Demo
 
-- [https://fc-polls.vercel.app/](https://fc-polls.vercel.app/)
-
-
-## Setup
-- After deploying your repo to Vercel...
-- Create a `kv` database `https://vercel.com/<name>/<project>/stores`
-- Set the `KV` prefix url's for the new `kv` database
-- Navigate to env variables: https://vercel.com/gregan/fc-links-vote/settings/environment-variables
-- If you're doing something production facing w/ trusted data, set the `HUB_URL` environment variable to a production hub's public ip address port 2283 ref: https://docs.farcaster.xyz/reference/frames/spec#frame-signature-packet
-- Set the `HOST` env variable to your public facing url or domain, ie; `https://<project>.vercel.app/`
--- app/page.tsx
+++ app/page.tsx
@@ -1,48 +1,38 @@
-import {PollCreateForm} from "./form";
+// ./app/page.tsx
 
-export let metadata = {
-  title: "Farcaster polls",
-  description: "Poll example for farcaster",
-};
+import {
+  FrameContainer,
+  FrameImage,
+  FrameButton,
+  useFramesReducer,
+  getPreviousFrame,
+  validateActionSignature,
+  FrameInput,
+} from "frames.js/next/server";
 
-function VercelLogo(props: React.SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      aria-label="Vercel Logo"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 19"
-      {...props}
-    >
-      <path
-        clipRule="evenodd"
-        d="M12.04 2L2.082 18H22L12.04 2z"
-        fill="#000"
-        fillRule="evenodd"
-        stroke="#000"
-        strokeWidth="1.5"
-      />
-    </svg>
+const reducer = (state, action) => ({ count: state.count + 1 });
+
+export default async function Home(props) {
+  const previousFrame = getPreviousFrame(props.searchParams);
+  const frameMessage = await getFrameMessage(previousFrame.postBody);
+
+  if (frameMessage && !frameMessage?.isValid) {
+    throw new Error("Invalid frame payload");
+  }
+  const [state, dispatch] = useFramesReducer(
+    reducer,
+    { count: 0 },
+    previousFrame
   );
-}
 
-export default async function Page() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <main className="flex flex-col items-center justify-center flex-1 px-4 sm:px-20 text-center">
-        <div className="flex justify-center items-center bg-black rounded-full w-16 sm:w-24 h-16 sm:h-24 my-8">
-          <VercelLogo className="h-8 sm:h-16 invert p-3 mb-1" />
-        </div>
-        <h1 className="text-lg sm:text-2xl font-bold mb-2">
-          Farcaster Polls
-        </h1>
-        <h2 className="text-md sm:text-xl mx-4">
-          Create a new poll with upto 4 options
-        </h2>
-        <div className="flex flex-wrap items-center justify-around max-w-4xl my-8 sm:w-full bg-white rounded-md shadow-xl h-full border border-gray-100">
-          <PollCreateForm />
-        </div>
-      </main>
-    </div>
+    <FrameContainer
+      postUrl="/frames"
+      state={state}
+      previousFrame={previousFrame}
+    >
+      <FrameImage src="https://picsum.photos/seed/frames.js/1146/600" />
+      <FrameButton onClick={dispatch}>{state.count}</FrameButton>
+    </FrameContainer>
   );
 }
