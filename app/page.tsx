7912487a9adf2b97795aa38479cff5f5c7beba2f
ivcained <86070833+ivcained@users.noreply.github.com>
2024-02-07 15:24:47 +0530

    chore: oh my god

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,35 @@
-// ./app/page.tsx
+import { getFrameMetadata } from "@coinbase/onchainkit";
+import type { Metadata } from "next";
 
-import {
-  FrameContainer,
-  FrameImage,
-  FrameButton,
-  useFramesReducer,
-  getPreviousFrame,
-  validateActionSignature,
-  FrameInput,
-} from "frames.js/next/server";
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: "Begin",
+    },
+  ],
+  image: `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmPeRY6bUTY4Cq3ukAfXPpC6tsTq5d1CUSmZBk9qadhd6V/00004-771275424.png`,
+  post_url: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=1`,
+});
 
-const reducer = (state, action) => ({ count: state.count + 1 });
+export const metadata: Metadata = {
+  title: "Just",
+  description: "Images",
+  openGraph: {
+    title: "While I work on Games",
+    description: "BasePlay",
+    images: [
+      `${process.env.NEXT_PUBLIC_GATEWAY_URL}/ipfs/QmPeRY6bUTY4Cq3ukAfXPpC6tsTq5d1CUSmZBk9qadhd6V/00004-771275424.png`,
+    ],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
 
-export default async function Home(props) {
-  const previousFrame = getPreviousFrame(props.searchParams);
-  const frameMessage = await getFrameMessage(previousFrame.postBody);
-
-  if (frameMessage && !frameMessage?.isValid) {
-    throw new Error("Invalid frame payload");
-  }
-  const [state, dispatch] = useFramesReducer(
-    reducer,
-    { count: 0 },
-    previousFrame
-  );
-
+export default function Page() {
   return (
-    <FrameContainer
-      postUrl="/frames"
-      state={state}
-      previousFrame={previousFrame}
-    >
-      <FrameImage src="https://picsum.photos/seed/frames.js/1146/600" />
-      <FrameButton onClick={dispatch}>{state.count}</FrameButton>
-    </FrameContainer>
+    <>
+      <h1>BasePlay</h1>
+    </>
   );
 }
